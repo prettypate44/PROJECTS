cfdeca451f7a1967efb74aac30c8a0150c5ac3c2
divine umebuali <umebualidivineeberechi@gmail.com>
2026-04-23 03:16:24 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+const taskRoutes = require('./routes/taskRoutes');
+
+
+//middleware
+app.use(express.json());
+
+//routes
+app.use('/api/tasks', taskRoutes);
+
+//server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+}); 
