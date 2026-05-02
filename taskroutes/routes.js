9d7b9d8802206b481f11a01c84ef046ef27678f8
divine umebuali <umebualidivineeberechi@gmail.com>
2026-05-02 15:19:18 +0100

    commit chan

--- taskroutes/routes.js
+++ taskroutes/routes.js
@@ -1,14 +1,32 @@
 //authroutes/ protcts
- const {verifyToken} = require("../middleware/taskroutes");
+ const express = require("express");
+const router = express.Router();
 
-router.post("/tasks", verifyToken, createTask);
-router.get("/tasks", verifyToken, getAllTasks)
+const {
+  createTask,
+  getAllTasks,
+  getTaskById,
+  updateTask,
+  deleteTask,
+} = require("../controllers/taskController");
+
+const { verifyToken } = require("../middleware/authMiddleware");
+
+const { validateTask } = require("../middleware/routes");
+
+// CREATE TASK
+router.post("/tasks", verifyToken, validateTask, createTask);
+
+// GET ALL TASKS
+router.get("/tasks", verifyToken, getAllTasks);
+
+// GET TASK BY ID
 router.get("/tasks/:id", verifyToken, getTaskById);
+
+// UPDATE TASK
 router.put("/tasks/:id", verifyToken, updateTask);
+
+// DELETE TASK
 router.delete("/tasks/:id", verifyToken, deleteTask);
 
-module.exports = router;//validation router
-
-const { validateTask } = require("../middleware/authMiddleware");
-
-router.post("/tasks", validateTask, createTask);
+module.exports = router;
